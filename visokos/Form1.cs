859ac22ba8fa7b9b8cab52d149d66138923e2962
Soborok1234 <sergei-goncharov2014@mail.ru>
2023-04-03 19:20:24 +0300

    Добавьте файлы проекта.

---
+++ visokos/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GodVis;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace visokos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double a;
+
+            a = double.Parse(textBox1.Text);
+
+
+            Class1 area = new Class1();
+            double result = area.GetArea(a);
+
+            if (result == 0) 
+            {
+                label1.Text = "Год Високосный";
+            }
+             else
+            {
+                label1.Text = "Год не Високосный";
+            }
+        }
+    }
+}
